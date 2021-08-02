e4cbc4b9edf8b1a9ffb838db26bcc77bd4d07b36
tommy <tommy.ling.dev@gmail.com>
2021-08-03 04:33:31 +1000

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+// @ts-nocheck
+
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh()],
+
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'lib/index.ts'),
+      name: 'Matter',
+      fileName: (format) => `lib.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
+});
